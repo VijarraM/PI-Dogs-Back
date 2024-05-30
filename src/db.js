a8704f21bcb024f045a53840e4cc040d521aa0f7
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-05-30 13:31:06 -0300

    actualización url db

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const Temperaments = require('./models/Temperament');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+// postgresql://postgres:PZGTiPYUpolvQxJvCMHBBDXdstxwEide@monorail.proxy.rlwy.net:57304/railway
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
